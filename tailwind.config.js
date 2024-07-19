e518e9d3bf740cd1c08b5d6413188f5a61c50f52
andijohan17 <162283801+andijohan17@users.noreply.github.com>
2024-07-19 14:50:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./publik/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors : {
+        background: '#336f6c',
+        color1: '#69BCB8',
+        white: '#F5F5F5',
+        login: '#336F6C',
+        hover: '#16403e',
+      },
+    },
+  },
+  plugins: [],
+}
+
